85be3a7ba07a5fec33041f17c68b296b422250ab
patrickRobotics <pirungu9@gmail.com>
2022-01-19 21:57:32 +0300

    Implement cli command to set initialize db

--- rptodo/config.py
+++ rptodo/config.py
@@ -1,13 +1,12 @@
-"""This module provides the RP To-Do config functionality."""
+"""This module provides the RP Todo config functionality."""
 # rptodo/config.py
 
 import configparser
 from pathlib import Path
-
 import typer
 
 from rptodo import (
-    DB_WRITE_ERROR, DIR_ERROR, FILE_ERROR, SUCCESS, __app_name_
+  DB_WRITE_ERROR, DIR_ERROR, FILE_ERROR, SUCCESS, __app_name__
 )
 
 CONFIG_DIR_PATH = Path(typer.get_app_dir(__app_name__))
