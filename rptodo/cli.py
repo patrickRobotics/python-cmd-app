b01c6bae8e5090b7311b3fd672125b031d8e4bf8
patrickRobotics <pirungu9@gmail.com>
2022-01-20 17:39:42 +0300

    implement delete to-do cli command to remove a to-do item

--- rptodo/cli.py
+++ rptodo/cli.py
@@ -14,11 +14,8 @@ app = typer.Typer()
 @app.command()
 def init(
     db_path: str = typer.Option(
-        str(database.DEFAULT_DB_FILE_PATH),
-        "--db-path",
-        "-db",
-        prompt="to-do database location?",
-    ),
+        str(database.DEFAULT_DB_FILE_PATH), "--db-path", "-db",
+        prompt="to-do database location?",),
 ) -> None:
     """Initialize the to-do database."""
     app_init_error = config.init_app(db_path)
@@ -125,6 +122,38 @@ def set_done(todo_id: int = typer.Argument(...)) -> None:
             f"""to-do # {todo_id} "{todo['Description']}" completed!""",
             fg=typer.colors.GREEN,
         )
+
+
+@app.command()
+def remove(
+    todo_id: int = typer.Argument(...),
+    force: bool = typer.Option(False, "--force", "-f", help="Force deletion without confirmation.",),
+) -> None:
+    """Remove a to-do using its TODO_ID."""
+    todoer = get_todoer()
+
+    def _remove():
+        todo, error = todoer.remove(todo_id)
+        if error:
+            typer.secho(f'Removing to-do # {todo_id} failed with "{ERRORS[error]}"', fg=typer.colors.RED,)
+            raise typer.Exit(1)
+        else:
+            typer.secho(f"""to-do # {todo_id}: '{todo["Description"]}' was removed""", fg=typer.colors.GREEN,)
+
+    if force:
+        _remove()
+    else:
+        todo_list = todoer.get_todo_list()
+        try:
+            todo = todo_list[todo_id - 1]
+        except IndexError:
+            typer.secho("Invalid TODO_ID", fg=typer.colors.RED)
+            raise typer.Exit(1)
+        delete = typer.confirm(f"Delete to-do # {todo_id}: {todo['Description']}?")
+        if delete:
+            _remove()
+        else:
+            typer.echo("Operation canceled")
 
 
 def _version_callback(value: bool) -> None:
